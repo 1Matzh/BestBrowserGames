79180a581bed8202dba06248d8d4ffa1257d3f15
1Matzh <matheusbenasse@gmail.com>
2023-11-10 23:34:54 -0300

    Implmentado página do usuário funcionando

---
+++ src/components/C_InputField.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { FaEdit, FaSave, FaTimes } from "react-icons/fa";
+
+const InputField = ({ label, value, onChange }) => {
+    const [isEditing, setEditing] = useState(false);
+    const [editedValue, setEditedValue] = useState(value);
+
+    const handleEditClick = () => {
+        setEditing(true);
+    };
+
+    const handleSaveClick = () => {
+        setEditing(false);
+        onChange(editedValue);
+    };
+
+    const handleCancelClick = () => {
+        setEditing(false);
+        setEditedValue(value);
+    };
+
+    return (
+        <div className="input-field">
+            <label>{label}:</label>
+            {isEditing ? (
+                <div className="edit-input">
+                    <input
+                        type="text"
+                        value={editedValue}
+                        onChange={(e) => setEditedValue(e.target.value)}
+                    />
+                    <FaSave onClick={handleSaveClick} />
+                    <FaTimes onClick={handleCancelClick} />
+                </div>
+            ) : (
+                <div className="edit-input">
+                    {value}
+                    <FaEdit onClick={handleEditClick} />
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default InputField;
--- src/pages/P_Profile.jsx
+++ src/pages/P_Profile.jsx
@@ -1,11 +1,82 @@
-import React from "react";
+// P_Perfil.jsx
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Cookies from "universal-cookie";
+import { jwtDecode } from "jwt-decode";
 
-const P_Profile = () => {
+import InputField from "../components/C_InputField";
+
+const P_Perfil = () => {
+  const cookies = new Cookies();
+  const token = cookies.get("token");
+
+  const [user, setUser] = useState({
+    _id: "",
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    birthDate: "",
+    country: "",
+    state: "",
+  });
+
+  useEffect(() => {
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      console.log(decodedToken);
+      
+      setUser({
+        _id: decodedToken.id,
+        name: decodedToken.name,
+        email: decodedToken.email,
+        password: "***",
+        confirmPassword: "***",
+        birthDate: decodedToken.birthDate,
+        country: decodedToken.country,
+        state: decodedToken.state,
+      });
+    }
+  }, [token]);
+  
+  const handleFieldChange = (fieldName, fieldValue) => {
+    // Always set the confirmPassword to the password value
+    setUser((prevUser) => ({
+      ...prevUser,
+      [fieldName]: fieldValue,
+    }));
+  };
+
+  const handleSaveClick = () => {
+    // Implement the save logic here
+    // This might include the PUT API call to update user information
+    axios.put(`https://bestbrowsergamesapi--1matzh.repl.co/users/${user._id}`, user, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then(response => {
+      console.log("User details saved successfully:", response.data);
+    })
+    .catch(error => {
+      console.error("Error saving user details:", error.response.data);
+    });
+  };
+  
   return (
-    <div>
-      <h2>Página de Perfil</h2>
+    <div className="profile-container">
+      <h2>Perfil do Usuário</h2>
+      <InputField label="Name" value={user.name} onChange={(value) => handleFieldChange("name", value)} />
+      <InputField label="Email" value={user.email} onChange={(value) => handleFieldChange("email", value)} />
+      <InputField label="Password" value={user.password} onChange={(value) => handleFieldChange("password", value)} />
+      <InputField label="Confirm Password" value={user.confirmPassword} onChange={(value) => handleFieldChange("confirmPassword", value)} />
+      <InputField label="Data de Nascimento" value={user.birthDate} onChange={(value) => handleFieldChange("birthDate", value)} />
+      <InputField label="País" value={user.country} onChange={(value) => handleFieldChange("country", value)} />
+      <InputField label="Estado" value={user.state} onChange={(value) => handleFieldChange("state", value)} />
+
+      <button className="btn btn-primary " onClick={handleSaveClick}>Salvar</button>
     </div>
   );
 };
 
-export default P_Profile;
+export default P_Perfil;
