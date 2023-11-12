9be33ce11ff2ee3b7a37f02981b37c92ac5d1e9e
1Matzh <matheusbenasse@gmail.com>
2023-11-12 19:11:09 -0300

    Removido importação desnecessária no componente EditReview

--- src/components/C_EditReview.jsx
+++ src/components/C_EditReview.jsx
@@ -1,5 +1,4 @@
-// EditReview.jsx
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Cookies from "universal-cookie";
 
