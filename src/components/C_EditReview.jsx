a36b0468ab9276dfd6687d4e44e993e6218137a4
1Matzh <matheusbenasse@gmail.com>
2023-11-12 19:09:37 -0300

    Implementado editação de avaliações feitas pelo usuário

---
+++ src/components/C_EditReview.jsx
@@ -0,0 +1,63 @@
+// EditReview.jsx
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Cookies from "universal-cookie";
+
+const EditReview = ({ review, onUpdate }) => {
+  const cookies = new Cookies();
+  const token = cookies.get("token");
+
+  const [editedScore, setEditedScore] = useState(review.score);
+  const [editedDescription, setEditedDescription] = useState(review.description);
+
+  const handleUpdateReview = () => {
+    if (!token) {
+      console.error("Usuário não autenticado. Não é possível editar a avaliação.");
+      return;
+    }
+
+    const updatedReview = {
+      score: editedScore,
+      description: editedDescription,
+      game: review.game._id,
+      user: review.user._id,
+    };
+
+    axios.put(`https://bestbrowsergamesapi--1matzh.repl.co/ratings/${review._id}`, updatedReview, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then(response => {
+      console.log("Avaliação atualizada com sucesso:", response.data);
+      onUpdate();
+    })
+    .catch(error => {
+      console.error("Erro ao atualizar a avaliação:", error.response.data);
+    });
+  };
+
+  return (
+    <div>
+      <h3>Editar Avaliação</h3>
+      <div>
+        <label>Nota:</label>
+        <input
+          type="number"
+          value={editedScore}
+          onChange={(e) => setEditedScore(Number(e.target.value))}
+        />
+      </div>
+      <div>
+        <label>Descrição:</label>
+        <textarea
+          value={editedDescription}
+          onChange={(e) => setEditedDescription(e.target.value)}
+        />
+      </div>
+      <button onClick={handleUpdateReview}>Salvar Alterações</button>
+    </div>
+  );
+};
+
+export default EditReview;
--- src/components/C_GameReviews.jsx
+++ src/components/C_GameReviews.jsx
@@ -1,8 +1,13 @@
+// GameReviews.jsx
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import C_EditReview from "./C_EditReview";
+import Cookies from "universal-cookie";
+import { jwtDecode } from "jwt-decode";
 
 const GameReviews = ({ gameId }) => {
   const [reviews, setReviews] = useState([]);
+  const [editingReviewId, setEditingReviewId] = useState(null);
 
   useEffect(() => {
     axios.get(`https://bestbrowsergamesapi--1matzh.repl.co/games/${gameId}/ratings`)
@@ -10,9 +15,44 @@ const GameReviews = ({ gameId }) => {
         setReviews(response.data);
       })
       .catch(error => {
-        console.error('Error fetching game reviews:', error);
+        console.error('Error fetching game reviews:', error.response.data);
       });
   }, [gameId]);
+
+  const handleUpdateReviews = () => {
+    setEditingReviewId(null);
+  };
+
+  const handleEditReview = (reviewId) => {
+    setEditingReviewId(reviewId);
+  };
+
+  const renderReview = (review) => {
+    const cookies = new Cookies();
+    const loggedInUserId = cookies.get("token") ? jwtDecode(cookies.get("token")).id : null;
+
+    if (review._id === editingReviewId) {
+      return (
+        <C_EditReview
+          key={review._id}
+          review={review}
+          onUpdate={handleUpdateReviews}
+        />
+      );
+    } else {
+      const isCurrentUserReview = loggedInUserId === review.user._id;
+
+      return (
+        <div key={review._id}>
+          <strong>{review.user.name}</strong> - Nota: {review.score}/5<br />
+          {review.description}<br />
+          {isCurrentUserReview && (
+            <button onClick={() => handleEditReview(review._id)}>Editar</button>
+          )}
+        </div>
+      );
+    }
+  };
 
   return (
     <div>
@@ -21,12 +61,7 @@ const GameReviews = ({ gameId }) => {
         <p>Nenhuma avaliação disponível.</p>
       ) : (
         <ul>
-          {reviews.map(review => (
-            <li key={review._id}>
-              <strong>{review.user.name}</strong> - Nota: {review.score}/5<br />
-              {review.description}
-            </li>
-          ))}
+          {reviews.map(renderReview)}
         </ul>
       )}
     </div>
@@ -34,3 +69,4 @@ const GameReviews = ({ gameId }) => {
 };
 
 export default GameReviews;
+
